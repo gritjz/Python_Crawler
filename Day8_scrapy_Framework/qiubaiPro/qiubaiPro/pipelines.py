5e426e471bd1d7e4fb9858dc2b153bb505eff38c
JNZhang <zjn811@gmail.com>
2021-03-16 23:39:06 -0400

    Scrapy: Part 2

--- Day8_scrapy_Framework/qiubaiPro/qiubaiPro/pipelines.py
+++ Day8_scrapy_Framework/qiubaiPro/qiubaiPro/pipelines.py
@@ -7,25 +7,60 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+import pymysql
 
 class QiubaiproPipeline:
     fp = None
-    #重写父类的方法： 该方法旨在开始爬虫的时候被调用一次
-    def open_spider(self,spider):
-        print('开始爬虫......')
+
+    # 重写父类的方法： 该方法旨在开始爬虫的时候被调用一次
+    def open_spider(self, spider):
+        print('START SPIDER......')
         self.fp = open('./qiubai.txt', 'w', encoding='utf-8')
 
-    #专门用来处理item对象
-    #该方法可以接受爬虫文件提交过来的item对象，见参数item
-    #该方法每当yield接收到一个item就会被调用一次
+    # 专门用来处理item对象
+    # 该方法可以接受爬虫文件提交过来的item对象，见参数item
+    # 该方法每当yield接收到一个item就会被调用一次
     def process_item(self, item, spider):
         author = item['author']
         content = item['content']
-
-        self.fp.write(author+': '+content+'\n')
-
+        self.fp.write(author + ':' + content)
         return item
 
-    def close_spider(self,spider):
-        print('FINISH SPIDER！')
+    def close_spider(self, spider):
         self.fp.close()
+        print('FINISH SPIDER！')
+
+#pipeline文件中, 一个类对应将一组数据存储到一个平台或者载体中
+class mysqlPipeLine(object):
+    conn = None
+    cursor = None
+    def open_spider(self, spider):
+        self.conn = pymysql.Connect(host='127.0.0.1',
+                                    port=3306,
+                                    user='root',
+                                    password='123456',
+                                    db='qiubai')
+
+    def process_item(self, item, spider):
+        self.cursor = self.conn.cursor()
+        try:
+            self.cursor.execute('insert into qiubai values("%s","%s")%(item["author"],item["content"])')
+        except Exception as e:
+            print(e)
+            self.conn.rollback()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Day8_scrapy_Framework/qiubaiPro/qiubaiPro/spiders/qiubai.py
+++ Day8_scrapy_Framework/qiubaiPro/qiubaiPro/spiders/qiubai.py
@@ -1,12 +1,14 @@
 import scrapy
 from qiubaiPro.items import QiubaiproItem
+
+
 #
 class QiubaiSpider(scrapy.Spider):
     name = 'qiubai'
     # allowed_domains = ['www.xxx.com']
     start_urls = ['https://www.qiushibaike.com/text/']
 
-    #1. 基于指令的持久化存储 -o
+    # 1. 基于指令的持久化存储 -o
     # def parse(self, response):
     #     # 解析：作者名称和段子的内容
     #     div_list = response.xpath('//*[@class="content-block clearfix"]/div[2]/div')
@@ -26,7 +28,7 @@ class QiubaiSpider(scrapy.Spider):
     #         all_data.append(dic)
     #     return all_data
 
-    #基于pipeline的持久化存储
+    # 基于pipeline的持久化存储
     def parse(self, response):
         # 解析：作者名称和段子的内容
         div_list = response.xpath('//*[@class="content-block clearfix"]/div[2]/div')
@@ -34,15 +36,20 @@ class QiubaiSpider(scrapy.Spider):
         for div in div_list:
             # xpath返回的是列表，但是列表元素一定是selector类型对象
             # extract()可以将selector对象中data参数存储的字符串提取出来
-            author = div.xpath('./div[1]/a[2]/h2/text() | ./div[1]/span/h2/text()').extract()
+            author = div.xpath('./div[1]/a[2]/h2/text()').extract()
+            author = ''.join(author)
+            #author = author.strip('\n')
             # 列表调用了.extract()之后，则表示将列表中每一个selector对象中data对应的字符串提取出来
-            content = div.xpath('./a[1]/div/span//text()').extract()
-            #content = ''.join(content)
+            content = div.xpath('./a[1]/div/span//text()|./div[1]/span/h2/text()').extract()
+            #content = content.strip('\n')
+
+            content = ''.join(content)
 
             item = QiubaiproItem()
             item['author'] = author
             item['content'] = content
 
-            #将item提交给pipeline
+            # 将item提交给pipeline
             yield item
+
 
